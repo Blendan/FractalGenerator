38b979270bdbf35549a4dfe703eed4f2a7dd42c6
Blendan <37484137+Blendan@users.noreply.github.com>
2019-03-17 20:45:14 +0100

    angefangen:
    custom image size

--- src/fractalGenerator/Controller.java
+++ src/fractalGenerator/Controller.java
@@ -1,13 +1,18 @@
 package fractalGenerator;
 
+import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.transform.Transform;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -19,6 +24,11 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable
 {
+	@FXML
+	public TextField inputResMultiplier;
+	public Label outResX;
+	public Label outResY;
+
 	@FXML
 	private Button btnGenerate;
 
@@ -52,6 +62,18 @@ public class Controller implements Initializable
 		fractalGenerator = new FractalGenerator(drawPane);
 		btnGenerate.setOnAction((e) -> startGenerating());
 		btnSave.setOnAction(event -> saveImage());
+
+		InvalidationListener scaleFeld = (e) -> new Thread(() -> Platform.runLater(this::setScale)).start();
+
+		drawPane.heightProperty().addListener(scaleFeld);
+		drawPane.widthProperty().addListener(scaleFeld);
+		setScale();
+	}
+
+	private void setScale()
+	{
+		outResX.setText(drawPane.getWidth() + "");
+		outResY.setText(drawPane.getHeight() + "");
 	}
 
 	private void startGenerating()
@@ -63,14 +85,13 @@ public class Controller implements Initializable
 			fractalGenerator.setStartLength(Integer.parseInt(inputHeight.getText()));
 			fractalGenerator.setFalloff(Double.parseDouble(inputFalloff.getText()));
 
-			if(!inputMargin.getText().equals(""))
+			if (!inputMargin.getText().equals(""))
 			{
 				fractalGenerator.setPaddingBottom(Double.parseDouble(inputMargin.getText()));
 			}
 
 			new Thread(fractalGenerator).start();
-		}
-		catch (Exception e)
+		} catch (Exception e)
 		{
 			System.out.println("input error");
 		}
@@ -79,19 +100,37 @@ public class Controller implements Initializable
 
 	private void saveImage()
 	{
-		SnapshotParameters snapshotParameters = new SnapshotParameters();
-		snapshotParameters.setFill(Color.BLACK);
-		FileChooser fileChooser = new FileChooser();
-		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("png files (*.png)", "*.png"));
-		File selectedFile = fileChooser.showSaveDialog(stage);
-
 		try
 		{
-			ImageIO.write(SwingFXUtils.fromFXImage(drawPane.snapshot(snapshotParameters,null), null), "png", selectedFile);
-		}
-		catch (IOException e)
+			int multiplier;
+			if(inputResMultiplier.getText().equals(""))
+			{
+				multiplier = Integer.parseInt(inputResMultiplier.getText());
+			}
+			else
+			{
+				multiplier = 1;
+			}
+
+			SnapshotParameters snapshotParameters = new SnapshotParameters();
+			snapshotParameters.setFill(Color.BLACK);
+			snapshotParameters.setTransform(Transform.scale(drawPane.getWidth()*multiplier, drawPane.getHeight()*multiplier));
+			WritableImage writableImage = new WritableImage((int)Math.rint(multiplier*drawPane.getWidth()), (int)Math.rint(multiplier*drawPane.getHeight()));
+
+			FileChooser fileChooser = new FileChooser();
+			fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("png files (*.png)", "*.png"));
+			File selectedFile = fileChooser.showSaveDialog(stage);
+
+			try
+			{
+				ImageIO.write(SwingFXUtils.fromFXImage(drawPane.snapshot(snapshotParameters, writableImage),null), "png", selectedFile);
+			} catch (IOException e)
+			{
+				System.out.println("no image selected");
+			}
+		} catch (Exception e)
 		{
-			System.out.println("no image selected");
+			e.printStackTrace();
 		}
 	}
 
