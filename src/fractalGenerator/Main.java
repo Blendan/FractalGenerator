f6470087689421479f2c7cab10366238960928c0
Blendan <37484137+Blendan@users.noreply.github.com>
2019-03-18 12:36:24 +0100

    icon hinzugefüght, bug gefunden und gefixd

--- src/fractalGenerator/Main.java
+++ src/fractalGenerator/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application
@@ -14,6 +15,7 @@ public class Main extends Application
 	{
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("mainGui.fxml"));
 		Parent root = loader.load();
+		primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("icon.png")));
 		primaryStage.setTitle("Fractal Generator");
 		primaryStage.setScene(new Scene(root));
 		primaryStage.show();
